1b93849b2bd5eb7639481641eed64840d3908ec1
Michael <maicol2014gb@gmail.com>
2022-08-22 13:43:40 -0500

    Interfaz 1.0

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -1,4 +1,5 @@
-from tkinter import FALSE
+from glob import glob
+import imp
 import cv2
 import os
 from keras.models import load_model
@@ -6,19 +7,37 @@ import numpy as np
 from pygame import mixer
 import time
 import os
+from timeit import default_timer
+
+from PIL import Image, ImageTk
+import tkinter as tk
+import imutils
 
 #Ubicar la ejecución en la carpeta actual del proyecto
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+ventana = tk.Tk()
+ventana.geometry("1000x680+200+10")
+ventana.title("tkinder-opencv")
+ventana.resizable(width=False,height=False)
+fondo = tk.PhotoImage(file="marco.png")
+fondo1= tk.Label(ventana,image=fondo).place (x=0,y=0,relwidth=1,relheight=1)
+video= cv2.VideoCapture(0)
+etiq_de_video = tk.Label(ventana, bg="black")
+etiq_de_video.place(x=187,y=75)
+tiempo = 0
+
 
 #Para reproducir la alarma cuando se duermen
 mixer.init()
 sound = mixer.Sound('alarm.wav')
 
-#Utilizamos opencv con cascadeClassifier 
+#Utilizamos opencv con cascadeClassifier
 face = cv2.CascadeClassifier('haar cascade files/haarcascade_frontalface_alt.xml')
 leye = cv2.CascadeClassifier('haar cascade files/haarcascade_lefteye_2splits.xml')
 reye = cv2.CascadeClassifier('haar cascade files/haarcascade_righteye_2splits.xml')
-
+tiempo  = 0
 
 
 lbl=['Close','Open']
@@ -29,17 +48,19 @@ cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 count=0
 score=0
+global thicc
 thicc=2
 rpred=[99]
 lpred=[99]
 reproduciendo=False
 
-while(True):
-    
+def iniciar():
+    global rpred
+    global lpred
     ret, frame = cap.read()
     height,width = frame.shape[:2] 
 
-    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
     left_eye = leye.detectMultiScale(gray)
@@ -52,6 +73,7 @@ while(True):
 
     for (x,y,w,h) in right_eye:
         r_eye=frame[y:y+h,x:x+w]
+        global count
         count=count+1
         r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
         r_eye = cv2.resize(r_eye,(24,24))
@@ -67,6 +89,8 @@ while(True):
 
     for (x,y,w,h) in left_eye:
         l_eye=frame[y:y+h,x:x+w]
+
+        
         count=count+1
         l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
         l_eye = cv2.resize(l_eye,(24,24))
@@ -79,33 +103,42 @@ while(True):
         if(lpred[0]==0):
             lbl='Closed'
         break
-
+    global score
+    
+    
     if(rpred[0]==0 and lpred[0]==0):
+        
         score=score+1
         cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
     # if(rpred[0]==1 or lpred[0]==1):
     else:
+        
         score=score-1
         cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
     
         
     if(score<0):
+        global reproduciendo
+        reproduciendo=False
         score=0   
     cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    global thicc
+    
     if(score>15):
         #person is feeling sleepy so we beep the alarm
         cv2.imwrite(os.path.join(path,'image.jpg'),frame)
         try:
-            
-            if(reproduciendo == FALSE):
+            print("INTENTANDO")
+            if(reproduciendo == False):
                 print("REPRODUCIENDO")
                 sound.play()
+                
                 reproduciendo=True
             else:
-                print("YA se esta reproduciendo")
+                print("Ya se esta reproduciendo")
 
 
-            
+        
         except:  # isplaying = False
             pass
         if(thicc<16):
@@ -115,8 +148,16 @@ while(True):
             if(thicc<2):
                 thicc=2
         cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    
+    frame = imutils.resize(frame, width=640)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(frame)
+    image= ImageTk.PhotoImage(image=img)
+    etiq_de_video.configure(image=image)
+    etiq_de_video.image = image
+    etiq_de_video.after(10,iniciar)
+        
+iniciar()
+ventana.mainloop()
 cap.release()
 cv2.destroyAllWindows()
